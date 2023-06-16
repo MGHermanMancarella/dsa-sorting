1a1bf63e4e21fdf9a2eaf0efb272ed839c87df0c
Michael <mherman315@gmail.com>
2023-06-16 12:27:32 -0700

    bubbleSort done"

--- bubble.js
+++ bubble.js
@@ -1,3 +1,16 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
 
+  for (let outer = arr.length - 1; outer >= 0; outer--) {
+    for (let inner = 0; inner <= outer - 1; inner++) {
+      let first = arr[inner];
+      let next = arr[inner + 1];
+      if (first > next) {
+        const tupperware = next;
+        arr[inner + 1] = first;
+        arr[inner] = tupperware;
+      }
+    }
+  }
+  return arr;
+}
 module.exports = bubbleSort;
